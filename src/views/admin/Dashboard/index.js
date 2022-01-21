d633014555e493fb5c8585ffa66821c0d7cb8082
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-01-21 13:52:27 +0200

    initiating app

---
+++ src/views/admin/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+import Breadcrumbs from '@components/breadcrumbs'
+
+const Dashboard = () => {
+
+  return (
+    <Fragment>
+      <div>
+        dashboard page
+      </div>
+    </Fragment>
+  )
+}
+
+export default Dashboard
